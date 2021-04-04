dfcc4bcd8a5ac4bc4195557abf5e23e8b30cd804
Edwin202-droid <80547097+Edwin202-droid@users.noreply.github.com>
2021-04-04 18:23:23 -0500

    3er commit

---
+++ classes/usuario-lista.ts
@@ -0,0 +1,64 @@
+import { Usuario } from "./usuario";
+
+export class UsuariosLista{
+
+    private lista: Usuario[]= [];
+
+    constructor(){}
+
+    /* Agregar Usuario */
+    public agregar(usuario:Usuario){
+
+        this.lista.push(usuario);
+        console.log(this.lista);
+        return usuario;
+
+        
+    }
+
+    public actualizarNombre(id:string, nombre:string){
+
+        for(let usuario of this.lista){
+            if(usuario.id = nombre){
+            break;
+            }
+        }
+
+        console.log('====Actualizando Usuario=====');
+        console.log(this.lista);
+        
+        
+    }
+
+    //Obtener lista de usuario
+    public getLista(){
+        return this.lista;
+    }
+
+    //Obtener un solo usuario de la lista
+    
+    public getUsuario(id:string){
+        return this.lista.find(usuario =>{
+            return usuario.id === id;
+        });
+    }
+
+    //Obtener usuario en una sala en particular
+    public getUsuariosEnSala (sala:string){
+        return this.lista.filter(usuario =>{
+            return usuario.sala === sala;
+        });
+    }
+
+    //Borrar el usuario -> cuando deja el chat
+    public borrarUsuario(id:string){
+
+        const tempUsuario= this.getUsuario(id);
+
+        this.lista= this.lista.filter(usuario=>{
+            return usuario.id !== id;
+        });
+
+        return tempUsuario;
+    }
+}
---
+++ routes/router.ts
@@ -0,0 +1,69 @@
+
+import { Router, Request, Response } from 'express';
+import Server from '../classes/server';
+
+const router = Router();
+
+
+
+router.get('/mensajes', ( req: Request, res: Response  ) => {
+
+    res.json({
+        ok: true,
+        mensaje: 'Todo esta bien!!'
+    });
+
+});
+
+/* enviar mensaje para todos - salas */
+
+router.post('/mensajes', ( req: Request, res: Response  ) => {
+
+    const cuerpo = req.body.cuerpo;
+    const de     = req.body.de;
+    const payload ={
+        de,
+        cuerpo
+    };
+
+    const server = Server.instance;
+
+    server.io.emit('mensaje-nuevo',payload)
+
+    res.json({
+        ok: true,
+        cuerpo,
+        de
+    });
+
+});
+
+/* Para enviar mensaje privados */
+router.post('/mensajes/:id', ( req: Request, res: Response  ) => {
+
+    const cuerpo = req.body.cuerpo;
+    const de     = req.body.de;
+    const id     = req.params.id;
+    const payload ={
+        de,
+        cuerpo
+    }
+
+    const server = Server.instance;
+    
+    server.io.in(id).emit('mensaje-privado',payload);
+
+    res.json({
+        ok: true,
+        cuerpo,
+        de,
+        id
+    });
+
+});
+
+
+
+export default router;
+
+
